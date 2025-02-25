35cf68e938cadcf4aae7abe229b53f305792dfbd
Manuel <manuel.valdes@catolica.edu.sv>
2025-02-24 18:00:45 -0600

    models

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P01_2022PD651_2022VZ650_PARQUEO.Models;
+
+namespace P01_2022PD651_2022VZ650_PARQUEO.Controllers
+{
+    [Route("Usuarios/[controller]")]
+    [ApiController]
+    public class UsuariosController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Models/Usuarios.cs
+++ Models/Usuarios.cs
@@ -1,6 +1,19 @@
-﻿namespace P01_2022PD651_2022VZ650_PARQUEO.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P01_2022PD651_2022VZ650_PARQUEO.Models
 {
     public class Usuarios
     {
+        [Key]
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string correo { get; set; }
+        public string contrasena { get; set; }
+        public enum rol
+        {
+            cliente,
+            empleado
+        }
+
     }
 }
